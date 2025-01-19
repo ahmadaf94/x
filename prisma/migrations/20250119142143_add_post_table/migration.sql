46e0e1bcce4ed0dc8aa31658b9ad3d8e5eaf9476
ahmadaf94 <ahmad.afshari1373@gmail.com>
2025-01-19 15:22:31 +0100

    initialize project and database

---
+++ prisma/migrations/20250119142143_add_post_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "author" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "likes" INTEGER NOT NULL DEFAULT 0,
+    "reposts" INTEGER NOT NULL DEFAULT 0,
+    "views" INTEGER NOT NULL DEFAULT 0
+);
